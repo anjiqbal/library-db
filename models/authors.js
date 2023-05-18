c67e06ef86a12950d596dd4e33b7a45282471598
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-05-18 15:44:32 +0100

    added searchAuthorsByName function

--- models/authors.js
+++ models/authors.js
@@ -2,14 +2,21 @@ import { pool } from "../db/index.js";
 
 export async function getAuthors() {
   // Query the database and return all authors
-  const result = await pool.query("SELECT * FROM authors")
+  const result = await pool.query("SELECT * FROM authors");
   return result.rows;
 }
 
 export async function searchAuthorsByName(searchTerm) {
   // Query the database and return all authors that have a name matching the searchTerm
-  return [];
+  const values = [searchTerm];
+  const query = `SELECT * FROM authors WHERE first_name = $1 OR LAST_NAME = $1`;
+  const result = await pool.query(query, values);
+  return result.rows;
 }
+
+// const values = [2, 3];
+// const query = `SELECT * FROM BOOKS WHERE id IN ($1, $2);`;
+// const result = await client.query(query, values);
 
 export async function getAuthorById(id) {
   // Query the database and return the book with a matching id
