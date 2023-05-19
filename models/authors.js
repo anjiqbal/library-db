9b9c450a5cdaba0e2fc13e198327e05a868c7f03
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-05-19 22:53:04 +0100

    initial commit

--- models/authors.js
+++ models/authors.js
@@ -14,10 +14,6 @@ export async function searchAuthorsByName(searchTerm) {
   return result.rows;
 }
 
-// const values = [2, 3];
-// const query = `SELECT * FROM BOOKS WHERE id IN ($1, $2);`;
-// const result = await client.query(query, values);
-
 export async function getAuthorById(id) {
   // Query the database and return the book with a matching id
   const values = [id];
@@ -27,17 +23,18 @@ export async function getAuthorById(id) {
 }
 
 export async function createAuthor(author) {
-  // Query the database to create an author and return the newly created author
-  const value = [author];
-  console.log(author)
-  const query = `INSERT INTO authors (first_name, last_name) VALUES (author.first_name, author.last_name)`;
-  const result = await pool.query(query, value);
+  const values = [author.first_name, author.last_name];
+  const query = `INSERT INTO authors (first_name, last_name) VALUES ($1, $2) RETURNING *`;
+  const result = await pool.query(query, values);
   return result.rows;
 }
 
 export async function updateAuthorById(id, updates) {
   // Query the database to update an author and return the newly updated author
-  return {};
+  const values = [id, updates.first_name, updates.last_name ]
+  const query = `UPDATE authors SET first_name = $2, last_name = $3 WHERE id = $1`
+  const result = await pool.query(query, values)
+  return result.rows;
 }
 
 export async function deleteAuthorById(id) {
